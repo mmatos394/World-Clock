75a85104ccadaa0a8d95a5798a3571142b9ea2f4
mmatos394 <126423898+mmatos394@users.noreply.github.com>
2023-03-08 17:18:27 +0000

    Added return home link

--- javascript/index.js
+++ javascript/index.js
@@ -45,7 +45,8 @@ function updateCity(event) {
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
           </div>
           <div class="time">${cityTime.format("HH:mm:ss")}</div>
-        </div>
+        </div> 
+        <a href="/">All cities</a>
         `;
 }
 
